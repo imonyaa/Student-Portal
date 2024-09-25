1e8414a963a9c82f63ffa26bf9708834d0ed575c
Imane Otmanine <otmanine.imane@gmail.com>
2024-09-25 16:57:33 +0100

    final fixes nchallah

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./state/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/Student-Portal">
+    <BrowserRouter basename="/Student-Portal">
       <Provider store={store}>
         <App />
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
